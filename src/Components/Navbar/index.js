41319a1a8aaf7da0a07b3e1fd5efc7d54a970531
dineshtiwari69 <210343@softwarica.edu.np>
2022-12-02 16:16:42 +0545

    Toggle Sidebar Responsiveness

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,10 +1,14 @@
+import React from "react";
+import { MenuIcon } from "@heroicons/react/outline";
 
-import React from "react"
-
-export default function Navbar(){
-    return(
-        <nav className="flex items-center justify-between flex-wrap shadow p-6 ">
-           
-            </nav>
-    )
+export default function Navbar() {
+    function toggleSidebar(){
+        let sideBarDom = document.getElementById("sideBarAside");
+        sideBarDom.classList.toggle("hidden");
+    }
+  return (
+    <nav className="flex items-center justify-between flex-wrap shadow p-6 ">
+      <MenuIcon className="h-5 text-black md:hidden" onClick={toggleSidebar}/>
+    </nav>
+  );
 }
